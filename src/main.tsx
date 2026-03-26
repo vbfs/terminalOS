133244c00a00c3303e4efd3a13defff590719c7e
vbfs <viniciusbarreto.me@gmail.com>
2026-03-26 11:55:26 -0300

    fix xterm css import and skip icons on CI

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import 'xterm/css/xterm.css'
+import '@xterm/xterm/css/xterm.css'
 import './styles/global.css'
 
 const root = document.getElementById('root')
