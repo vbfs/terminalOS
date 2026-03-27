4813fd0ec04aa5ee0887807781df90deb82ca31f
vbfs <viniciusbarreto.me@gmail.com>
2026-03-26 22:57:57 -0300

    try to fix windows path

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+// Polyfill crypto.randomUUID for HTTP (non-secure) contexts
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  (crypto as any).randomUUID = (): string => {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) => {
+      const n = +c
+      return (n ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (n / 4)))).toString(16)
+    })
+  }
+}
+
 import './lib/api-bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
